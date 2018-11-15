6fd8ad5e32642f1f91fd37ef12692fe8bec526a9
nijashansen <nijas.w.hansen@gmail.com>
2018-11-15 11:52:28 +0100

    whup

--- src/movierecsys/dal/RatingDAO.java
+++ src/movierecsys/dal/RatingDAO.java
@@ -9,11 +9,14 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import movierecsys.be.Movie;
 import movierecsys.be.Rating;
@@ -47,18 +50,65 @@ public class RatingDAO
      * Updates the rating to reflect the given object.
      * @param rating The updated rating to persist.
      */
-    public void updateRating(Rating rating)
+    public void updateRating(Rating rating) throws IOException
     {
-        //TODO Update rating
+        File tmp = new File("data/tmp_movies.txt");
+        List<Rating> allRating = getAllRatings();
+        allRating.removeIf((Rating t) -> t.getRating()== rating.getRating());
+        allRating.add(rating);
+        Collections.sort(allRating, (Rating o1, Rating o2) -> Integer.compare(o1.getRating(), o2.getRating()));
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(tmp)))
+        {
+            for (Rating rat : allRating)
+            {
+                bw.write(rat.getMovie()+","+rat.getUser()+","+rat.getRating());
+                bw.newLine();
+            }
+        }
+        Files.copy(tmp.toPath(), new File(RATING_SOURCE).toPath(), StandardCopyOption.REPLACE_EXISTING);
+        Files.delete(tmp.toPath());
     }
     
     /**
      * Removes the given rating.
      * @param rating 
      */
-    public void deleteRating(Rating rating)
+    public void deleteRating(Rating rating) throws IOException
     {
-        //TODO Delete rating
+        String tempFile = "temp.txt";
+        File oldFile = new File (RATING_SOURCE);
+        File newFile = new File (tempFile);
+        List<Rating> newRatList = new ArrayList();
+        List<Rating> oldRatList = getAllRatings();
+        try
+        {
+            for (int i = 0; i < oldRatList.size(); i++)
+            {
+                if(oldRatList.get(i).getRating()!= rating.getRating())
+                {
+                    newRatList.add(oldRatList.get(i));
+                }
+            }
+            FileWriter fw = new FileWriter(tempFile);
+            BufferedWriter bw = new BufferedWriter(fw);
+            for (Rating rate : newRatList)
+            {
+                bw.write(rate.getMovie()+ "," + rate.getUser() + "," + rate.getRating());
+                bw.newLine();
+            }
+            bw.flush();
+            bw.close();
+            oldFile.delete();
+            File dump = new File(RATING_SOURCE);
+            newFile.renameTo(dump);
+        }
+        catch(IOException e)
+                {
+                    System.out.println("Something went wrong");
+                }
+        
+        
+        
     }
     
     /**
@@ -99,10 +149,35 @@ public class RatingDAO
      */
     public int getRatings(User user) throws IOException
     {
-        List<Rating> allRatings = getAllRatings();
-        int highId = allRatings.get(allRatings.size() - 1).getRating();
-        return highId + 1;
+        List<Rating> allRates = new ArrayList<>();
+        String source = "data/Ratings.txt";
+        File file = new File(source);
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) //Using a try with resources!
+        {
+            String line;
+            while ((line = reader.readLine()) != null)
+            {
+                if (!line.isEmpty())
+                {
+                    try
+                    {
+                        Rating rate = stringArrayToRating(line);
+                        if (rate.getRating() == getRatings(user))
+                        {
+                           return getRatings(user);
+                        }
+                        
+                    } catch (Exception ex)
+                    {
+                        //Do nothing. Optimally we would log the error.
+                    }
+                }
+            }
+        }
+        return getRatings(user);
     }
+    
     
     private Rating stringArrayToRating(String line)
     {
