3996f6f8ff9e73bbb7853bd328d6d062e82e727b
nijashansen <nijas.w.hansen@gmail.com>
2018-11-15 11:15:12 +0100

    added lidt, ved ikke om det virker

--- src/movierecsys/dal/RatingDAO.java
+++ src/movierecsys/dal/RatingDAO.java
@@ -5,7 +5,17 @@
  */
 package movierecsys.dal;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 import java.util.List;
+import movierecsys.be.Movie;
 import movierecsys.be.Rating;
 import movierecsys.be.User;
 
@@ -15,14 +25,22 @@ import movierecsys.be.User;
  */
 public class RatingDAO
 {
+    private static final String RATING_SOURCE = "data/ratings.txt";
     
     /**
      * Persists the given rating.
      * @param rating the rating to persist.
      */
-    public void createRating(Rating rating)
+    public void createRating(Rating rating, User user) throws IOException
     {
-        //TODO Rate movie
+        Path path = new File(RATING_SOURCE).toPath();
+        int id = -1;
+        try (BufferedWriter bw = Files.newBufferedWriter(path, StandardOpenOption.SYNC, StandardOpenOption.APPEND, StandardOpenOption.WRITE))
+        {
+            id = getRatings(user);
+            bw.newLine();
+            bw.write(id + "," + user + "," + rating);
+        }
     }
     
     /**
@@ -47,10 +65,31 @@ public class RatingDAO
      * Gets all ratings from all users.
      * @return List of all ratings.
      */
-    public List<Rating> getAllRatings()
+    public List<Rating> getAllRatings() throws IOException
     {
-        //TODO Get all rating.
-        return null;
+        List<Rating> allRatings = new ArrayList<>();
+        String source = "data/ratings.txt";
+        File file = new File(source);
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) //Using a try with resources!
+        {
+            String line;
+            while ((line = reader.readLine()) != null)
+            {
+                if (!line.isEmpty())
+                {
+                    try
+                    {
+                        Rating rat = stringArrayToRating(line);
+                        allRatings.add(rat);
+                    } catch (Exception ex)
+                    {
+                        //Do nothing. Optimally we would log the error.
+                    }
+                }
+            }
+        }
+        return allRatings;
     }
     
     /**
@@ -58,10 +97,22 @@ public class RatingDAO
      * @param user The user 
      * @return The list of ratings.
      */
-    public List<Rating> getRatings(User user)
+    public int getRatings(User user) throws IOException
     {
-        //TODO Get user ratings.
-        return null;
+        List<Rating> allRatings = getAllRatings();
+        int highId = allRatings.get(allRatings.size() - 1).getRating();
+        return highId + 1;
     }
     
+    private Rating stringArrayToRating(String line)
+    {
+        String[] arrRating = line.split(",");
+
+        int id = Integer.parseInt(arrRating[0]);
+        int user = Integer.parseInt(arrRating[1]);
+        String movie = arrRating[2];
+
+        Rating rat = new Rating(movie, user, id);
+        return rat;
+    }
 }
